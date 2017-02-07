7bb4bc14626493a3113916f6b0034012f69b7fc1
Gökhan Karagöz <annotto3@gmail.com>
2017-02-07 11:11:17 +0300

    Create Player object from classes.

--- SuperAdventure/SuperAdventure/SuperAdventure.Designer.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.Designer.cs
@@ -36,7 +36,6 @@
             this.lblGold = new System.Windows.Forms.Label();
             this.lblExperience = new System.Windows.Forms.Label();
             this.lblLevel = new System.Windows.Forms.Label();
-            this.btnTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -111,22 +110,11 @@
             this.lblLevel.TabIndex = 7;
             this.lblLevel.Text = "0";
             // 
-            // btnTest
-            // 
-            this.btnTest.Location = new System.Drawing.Point(21, 129);
-            this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(102, 23);
-            this.btnTest.TabIndex = 8;
-            this.btnTest.Text = "Test";
-            this.btnTest.UseVisualStyleBackColor = true;
-            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
-            // 
             // SuperAdventure
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 651);
-            this.Controls.Add(this.btnTest);
             this.Controls.Add(this.lblLevel);
             this.Controls.Add(this.lblExperience);
             this.Controls.Add(this.lblGold);
@@ -152,7 +140,6 @@
         private System.Windows.Forms.Label lblGold;
         private System.Windows.Forms.Label lblExperience;
         private System.Windows.Forms.Label lblLevel;
-        private System.Windows.Forms.Button btnTest;
     }
 }
 
--- SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -8,18 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Engine;
+
 namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
+        private Player _player;
+
         public SuperAdventure()
         {
             InitializeComponent();
-        }
 
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lblGold.Text = "123";
+            _player = new Player();
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
     }
 }
