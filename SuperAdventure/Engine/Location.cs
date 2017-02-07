f93a1da74b6f5319d1b2ccb28b50eb36293ffe02
Gökhan Karagöz <annotto3@gmail.com>
2017-02-07 12:03:08 +0300

    Use classes as datatypes

--- SuperAdventure/Engine/Location.cs
+++ SuperAdventure/Engine/Location.cs
@@ -12,11 +12,25 @@ namespace Engine
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public Location(int id, string name, string description)
+        public Item ItemRequiredToEnter { get; set; }
+        public Quest QuestAvailableHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocationToNorth { get; set; }
+        public Location LocationToEast { get; set; }
+        public Location LocationToSouth { get; set; }
+        public Location LocationToWest { get; set; }
+
+        public Location(int id, string name, string description, 
+            Item itemRequiredToEnter = null, 
+                Quest questAvailableHere = null,
+                    Monster monsterLivingHere = null)
         {
             ID = id;
             Name = name;
             Description = description;
+            ItemRequiredToEnter = itemRequiredToEnter;
+            QuestAvailableHere = questAvailableHere;
+            MonsterLivingHere = monsterLivingHere;
         }
     }
 }
--- SuperAdventure/Engine/Quest.cs
+++ SuperAdventure/Engine/Quest.cs
@@ -13,6 +13,7 @@ namespace Engine
         public string Description { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+        public Item RewardItem { get; set; }
 
         public Quest(int id, string name, string description,
             int rewardExperiencePoints, int rewardGold)
