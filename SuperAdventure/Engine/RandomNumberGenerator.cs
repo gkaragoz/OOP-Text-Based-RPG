fd2ad1cad37470f68135bb4b4700a7e8e02d8eea
Gökhan Karagöz <annotto3@gmail.com>
2017-02-07 13:41:16 +0300

    Add GenerateRandomNumber class.

---
+++ SuperAdventure/Engine/RandomNumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        private static Random _generator = new Random();
+    
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            return _generator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
