362b4cf8bed1e60931f898e953953415320c96c0
Gökhan Karagöz <annotto3@gmail.com>
2017-02-07 11:03:23 +0300

    Create the Player class and its properties.

---
+++ SuperAdventure/Engine/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Player
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+    }
+}
